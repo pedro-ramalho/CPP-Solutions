a420b3153defb7c0afb0f209e937f7497b76db10
pedro-ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2021-02-26 08:31:10 +0000

    Add files via upload

---
+++ 1.1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char n;
+	cout << "Type a character: " << endl;
+	cin >> n;
+	cout << "The ASCII code of " << n << " is " << int(n);
+	return 0;
+}
