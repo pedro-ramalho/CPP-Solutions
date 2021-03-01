4fed68a25256ef3bfdc2ac7d2550a6c639d0fe11
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2021-03-01 14:24:46 +0000

    Create 2.7.c).cpp

---
+++ 2./2.7.c).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+bool prime(int n) {
+
+	if (n < 2) {
+		return false;
+	}
+	else if (n == 2) {
+		return true;
+	}
+	else {
+		for (int i = 2; i < n; i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+int main() {
+  int counter = 0, n = 0;
+
+	while (counter != 100) {
+		if (prime(n)) {
+			cout << n << endl;
+			counter++;
+		}
+		n++;
+	}
+
+	return 0;
+}
