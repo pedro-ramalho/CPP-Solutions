cbf0627083b78ec9ba7b74ee2f3ee88b6feb3552
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2021-03-01 21:27:58 +0000

    Update 2.10.cpp

--- 2./2.10.cpp
+++ 2./2.10.cpp
@@ -15,7 +15,7 @@ int main() {
         n = n / 2;
     }
 
-    for (int i = 3; i <= sqrt(n); i = i + 2) {
+    for (int i = 3; i <= sqrt(n); i += 2) {
         while (n % i == 0) {
             factors.push_back(i);
             n = n / i;
