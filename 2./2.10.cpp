762c1e90f0becc6436b846150ce1e16114078976
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2021-03-01 21:26:44 +0000

    Update 2.10.cpp

--- 2./2.10.cpp
+++ 2./2.10.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <math.h>
 using namespace std;
 
@@ -6,21 +7,34 @@ using namespace std;
 int main() {
 
     int n;
+    vector <int> factors;
+    
     cin >> n;
     while (n % 2 == 0) {
-        cout << 2 << " ";
+        factors.push_back(2);
         n = n / 2;
     }
 
     for (int i = 3; i <= sqrt(n); i = i + 2) {
         while (n % i == 0) {
-            cout << i << " ";
+            factors.push_back(i);
             n = n / i;
         }
     }
 
     if (n > 2) {
-        cout << n << " ";
+        factors.push_back(n);
+    }
+
+    int length = factors.size();
+
+    for (int j = 0; j < length; j++) {
+        if (j < length - 1) {
+            cout << factors[j] << "x";
+        }
+        else {
+            cout << factors[j];
+        }
     }
 
     return 0;
