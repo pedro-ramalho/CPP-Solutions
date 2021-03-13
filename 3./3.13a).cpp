ab2b2a3041ee3213b7ab5d2c274345e28b8c00d1
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2021-03-13 13:14:12 +0000

    Create 3.13a).cpp

---
+++ 3./3.13a).cpp
@@ -0,0 +1,11 @@
+/*
+int rollDie(int low = 1, int high = 6)
+	{
+	assert(high >= low);
+	return (rand() % (high - low + 1)) + low;
+}
+*/
+
+//This function generates a random integer between 1 and 6 (inclusive)
+
+//If the expression inside the assert is false, then an error occurs.
